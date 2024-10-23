5083fb867a6b4a1fbb0316ae5aea724bd8a32b67
44mira <legolaslada@gmail.com>
2024-10-23 22:43:26 +0800

    feat: use dataframe

--- lada_firefly.py
+++ lada_firefly.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
 from random import random
 from numpy import cos, sqrt
+
+
+# [[ Functions ]] {{{
 
 
 def griewank(dimensions):
@@ -19,21 +23,42 @@ def bounded_generate(lb, ub):
     return lb + (ub - lb) * random()
 
 
-bounds = -10, 10
+# }}}
 
-population, pop_size = [], 10
-dimension = 5
-fitnesses = []
 
-for k in range(pop_size):
-    solution = [bounded_generate(*bounds) for _ in range(dimension)]
-    population.append(solution)
+def main():
+    hr = lambda: print("-" * 32)
 
-    fitnesses.append(griewank(solution))
+    bounds = -10, 10
 
-# display values
-for i in range(pop_size):
-    print(f"Solution {i}: {population[i]}")
+    population, pop_size = [], 10
+    dimension = 5
+    fitnesses = []
 
-for i in range(pop_size):
-    print(f"Fitness {i}: {fitnesses[i]:.5f}")
+    for _ in range(pop_size):
+        solution = [bounded_generate(*bounds) for _ in range(dimension)]
+        population.append(solution)
+
+        fitnesses.append(griewank(solution))
+
+    data = []
+    for fitness, soln in zip(fitnesses, population):
+        tmp = soln
+        tmp.append(fitness)
+        data.append(tmp)
+
+    col_labels = [*(str(i) for i in range(1, dimension + 1)), "fitness"]
+    row_labels = [f"Soln {i}" for i in range(1, pop_size + 1)]
+    df = pd.DataFrame(data=data, columns=col_labels, index=row_labels)
+
+    hr()
+    print("Unsorted Population: ")
+    print(df)
+
+    hr()
+    print("Sorted Population: ")
+    print(df.sort_values("fitness"))
+
+
+if __name__ == "__main__":
+    main()
